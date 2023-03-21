e3fdd4e20da86e4bb845da61b1b86505029ec1a5
Abner Orlamunder <orlamunder.abner@gmail.com>
2023-03-21 10:39:23 -0300

    adding opensource text and info panel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,14 @@ import classNames from "classnames";
 import Head from "next/head";
 import Script from "next/script";
 import { useEffect, useState } from "react";
+import { FaInfoCircle } from "react-icons/fa";
 
 export default function Home() {
   const [apiKey, setApiKey] = useState("");
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<RoundData>();
   const [messages, setMessages] = useState([]);
+  const [infoTabOpened, setInfoTabOpened] = useState(false);
 
   const handleChooseCommand = async (command) => {
     setLoading(true);
@@ -37,41 +39,83 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col overflow-hidden items-center justify-center">
+      <a
+        href="https://github.com/agencyenterprise/sds-roleplaying-gpt"
+        rel="noreferrer"
+        target="_blank"
+        className="absolute bottom-6 right-6 text-[#E5E5E5] text-lg text-right"
+      >
+        Want to make the adventure better?
+        <br />
+        Contribute on GitHub!
+      </a>
       <div className="relative w-full my-6 max-w-2xl p-4">
         <div
           id="parchment"
           className="absolute top-0 right-0 bottom-0 left-0"
         />
 
-        <h1 className="inkTitle mb-6">Roleplaying GPT</h1>
-        {data ? (
-          <PlayerCard
-            data={data}
-            onChooseCommand={handleChooseCommand}
-            loading={loading}
-          />
-        ) : (
-          <div className="flex flex-col gap-2">
-            <span className="text-neutral-600">
-              Input your OpenAI API Key before start
-            </span>
-            <input
-              type="text"
-              value={apiKey}
-              onChange={(e) => setApiKey(e.target.value)}
-              placeholder="Key"
-              className="w-full rounded-md border border-orange-400 saturate-50 text-black placeholder:text-neutral-700 outline-none text-sm p-1"
-            />
+        <button
+          className="absolute top-6 right-6 z-10"
+          onClick={() => setInfoTabOpened((prev) => !prev)}
+        >
+          <FaInfoCircle />
+        </button>
+
+        {infoTabOpened && (
+          <div className="flex flex-col gap-4 p-8 pt-12">
+            <h1 className="mb-6 text-center text-xl">
+              Welcome adventurer!
+              <br />
+              You begin in a small town, and opportunity awaits!
+            </h1>
+            <h1 className="p-8 bg-[#CAAD8B]">
+              {`Notice: This is an experimental project using GPT, your adventure
+              might not work perfectly. If you run into issues, use the "other"
+              command to perform more accurate commands. Adventure resets upon
+              page refresh.`}
+            </h1>
             <button
-              className={classNames(
-                "opacity-50 hover:opacity-100",
-                loading && "animate-pulse"
-              )}
-              onClick={handleStart}
-              disabled={apiKey.length <= 0 || loading}
+              className="opacity-50 hover:opacity-100 text-lg"
+              onClick={() => setInfoTabOpened(false)}
             >
-              {loading ? "Starting..." : "Start your adventure"}
+              Continue to Adventure
             </button>
+          </div>
+        )}
+        {!infoTabOpened && (
+          <div className="flex flex-col gap-4 p-8 pt-12">
+            <h1 className="inkTitle">Roleplaying GPT</h1>
+            {data ? (
+              <PlayerCard
+                data={data}
+                onChooseCommand={handleChooseCommand}
+                loading={loading}
+              />
+            ) : (
+              <div className="flex flex-col gap-2">
+                <span className="text-neutral-600">
+                  Input your OpenAI API Key before start
+                </span>
+                <input
+                  type="text"
+                  value={apiKey}
+                  onChange={(e) => setApiKey(e.target.value)}
+                  placeholder="Key"
+                  className="w-full rounded-md border border-orange-400 saturate-50 text-black placeholder:text-neutral-700 outline-none text-sm p-1"
+                />
+                <button
+                  className={classNames(
+                    "opacity-50 hover:opacity-100 text-lg",
+                    loading && "animate-pulse"
+                  )}
+                  onClick={handleStart}
+                  disabled={apiKey.length <= 0 || loading}
+                >
+                  {loading ? "Starting..." : "Start your adventure"}
+                </button>
+              </div>
+            )}
           </div>
         )}
       </div>
