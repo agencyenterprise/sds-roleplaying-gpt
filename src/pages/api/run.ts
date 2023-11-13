4044600841cf86b3f36e2abc5e2e580364bab835
Abner Orlamunder <orlamunder.abner@gmail.com>
2023-11-13 15:17:33 -0300

    fixing import error

--- src/pages/api/run.ts
+++ src/pages/api/run.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { OpenAIApi, Configuration } from "openai";
+import { OpenAI } from "openai";
 
 export default async function handler(
   req: NextApiRequest,
@@ -12,8 +12,7 @@ export default async function handler(
     apiKey = process.env.OPENAI_API_KEY;
   }
 
-  const configuration = new Configuration({ apiKey });
-  const openai = new OpenAIApi(configuration);
+  const openai = new OpenAI({ apiKey });
 
   if (messages.length <= 0) {
     messages.push({
